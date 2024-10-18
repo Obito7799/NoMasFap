e3f2468753d4e24ee14fc3d937be5ba80d1724ca
Obito7799 <144813818+Obito7799@users.noreply.github.com>
2024-10-18 17:48:26 -0600

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// script.js  
+document.addEventListener('DOMContentLoaded', () => {  
+    console.log("Página cargada. Aquí puedes implementar más funciones.");  
+    // Puedes agregar funciones para mejorar la experiencia del usuario aquí  
+});
